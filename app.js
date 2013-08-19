ba5b5c36577da5157482d31d35c5989563058fb3
Bob Schmertz <rschmertz@42six.com>
2013-08-19 18:33:17 -0400

    Added socket.io to the mix

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app)
+, io = require('socket.io').listen(server);
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
