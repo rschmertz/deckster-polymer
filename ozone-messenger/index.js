34b29829b0bcd0c5c1250010aceeab46345c2f18
Bob Schmertz <rschmertz@42six.com>
2013-08-30 12:16:11 -0400

    Move some things around in index.js to make it possible for other node modules to do channel creation.

--- ozone-messenger/index.js
+++ ozone-messenger/index.js
@@ -1,36 +1,80 @@
+function obj_map(hash, fn) {
+    var keys = {}, key;
+    for (key in hash) {
+        
+        keys[key] = (typeof fn == 'function') ? fn(hash[key]) : {};
+    };
+    return keys;
+};
+
+// Create a simple hash that is JSONifiable
+function getFlatHash (hash) {
+    return obj_map(hash);
+}
+
+var io;
+
+var allSockets = {};
+// Keep track of all clients to make sure there are no duplicate IDs
+var messengerClients = {};
+var channels = {};
+
+function broadcastChannelList() {
+    var channelListOut = obj_map(channels, function (channel) {
+        return {
+            name: channel.name,
+            options: channel.options
+        }
+    });
+    io.sockets.emit('channelListUpdate', {channelList: channelListOut});
+};
+
+exports.createChannel =  function (channelName, creatorName, options, fn) {
+    if (channelName in channels || channelName in messengerClients) {
+        fn({error: "The name "+channelName+" is already in use"});
+        return;
+    };
+    channels[channelName] = { name: channelName,
+                              creatorName: creatorName,
+                              //socketid: socket.id,
+                              subscribers: {},
+                              options: options };
+
+    broadcastChannelList();
+};
+
+exports.sendMessage = function (targetID, data, fn) {
+    console.log('sendMessage called for %s with data ' + data.message, targetID);
+    try {
+        var targetObj = messengerClients[targetID];
+        var channel = channels[targetID];
+        if (targetObj) {
+            var targetSocket = allSockets[targetObj.socketid];
+            targetSocket.emit('receive message', targetID, data);
+        } else if (channel) {
+            console.log(targetID + " is a channel.  Whoopee.");
+            var targetSockets = {};
+            for (clientid in channel.subscribers) {
+                // Add the relevent socket for this client to the set
+                targetSockets[messengerClients[clientid].socketid] = 1
+            };
+            console.log("Target sockets are:");
+            console.dir(targetSockets);
+            for (var socketid in targetSockets) {
+                allSockets[socketid].emit('receive message', targetID, data);
+            };
+        };
+        fn({success: 'received sendMessage'});
+    } catch (e) {
+        var msg = "Error forwarding message: perhaps " + targetID + " does not exist";
+        console.log(e + ": " + msg);
+        fn({error: msg});
+    };
+};
+
 exports.listen = function (server) {
 
-    function obj_map(hash, fn) {
-        var keys = {}, key;
-        for (key in hash) {
-            
-            keys[key] = (typeof fn == 'function') ? fn(hash[key]) : {};
-        };
-        return keys;
-    };
-        
-    // Create a simple hash that is JSONifiable
-    function getFlatHash (hash) {
-        return obj_map(hash);
-    }
-
-    var io = require('socket.io').listen(server);
-
-    var allSockets = {};
-    // Keep track of all clients to make sure there are no duplicate IDs
-    var messengerClients = {};
-    var channels = {};
-
-    function broadcastChannelList() {
-        var channelListOut = obj_map(channels, function (channel) {
-            return {
-                name: channel.name,
-                options: channel.options
-            }
-        });
-        io.sockets.emit('channelListUpdate', {channelList: channelListOut});
-    };
-    
+    io = require('socket.io').listen(server);
     io.sockets.on('connection', function (socket) {
         
         // connectionClients keeps track of clients attached to this socket, so
@@ -53,19 +97,7 @@ exports.listen = function (server) {
             io.sockets.emit('clientListUpdate', {clientList: messengerClients});
         });
 
-        socket.on('create channel', function (channelName, creatorName, options, fn) {
-            if (channelName in channels || channelName in messengerClients) {
-                fn({error: "The name "+channelName+" is already in use"});
-                return;
-            };
-            channels[channelName] = { name: channelName,
-                                      creatorName: creatorName,
-                                      socketid: socket.id,
-                                      subscribers: {},
-                                      options: options };
-
-            broadcastChannelList();
-        });         
+        socket.on('create channel', exports.createChannel);
         socket.on('subscribe', function (channelName, clientName, fn) {
             var channel = channels[channelName];
             if (!channel) {
@@ -79,35 +111,7 @@ exports.listen = function (server) {
             };
             fn({ success: clientName + "Successfully subscribed to channel " + channelName});
         });
-        socket.on('sendMessage', function (targetID, data, fn) {
-            console.log('sendMessage called for %s with data ' + data.message, targetID);
-            try {
-                var targetObj = messengerClients[targetID];
-                var channel = channels[targetID];
-                if (targetObj) {
-                    var targetSocket = allSockets[targetObj.socketid];
-                    targetSocket.emit('receive message', targetID, data);
-                } else if (channel) {
-                    console.log(targetID + " is a channel.  Whoopee.");
-                    var targetSockets = {};
-                    for (clientid in channel.subscribers) {
-                        // Add the relevent socket for this client to the set
-                        targetSockets[messengerClients[clientid].socketid] = 1
-                    };
-                    console.log("Target sockets are:");
-                    console.dir(targetSockets);
-                    for (var socketid in targetSockets) {
-                        allSockets[socketid].emit('receive message', targetID, data);
-                    };
-                };
-                fn({success: 'received sendMessage'});
-            } catch (e) {
-                var msg = "Error forwarding message: perhaps " + targetID + " does not exist";
-                console.log(e + ": " + msg);
-                fn({error: msg});
-            };
-        });
-
+        socket.on('sendMessage', exports.sendMessage);
         socket.on('drop client', function dropClient (clientName) {
             if (typeof connectionClients[clientName] == 'undefined') {
                 console.log("Client %s does not belong to the socket requesting disconnect", clientName);
