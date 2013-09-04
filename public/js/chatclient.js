5486f1a925f17b6d68e215dd6f20f0755e9ff46c
Robert Schmertz <rschmertz@42six.com>
2013-09-04 00:44:34 -0400

    Show only channels in subscription dropdown.

--- public/js/chatclient.js
+++ public/js/chatclient.js
@@ -7,13 +7,20 @@ ChatClient = (function () {
             _component.messages.push({content: message.message});
         };
         this.clientsUpdated = function () {
-            var keylist = Messenger.allClients.getKeys().concat(Messenger.allChannels.getKeys());
+            var channelKeys = Messenger.allChannels.getKeys();
+            var keylist = Messenger.allClients.getKeys().concat(channelKeys);
             var objectList = []; // the Polymer list here apparently can't work with simple strings in an array, so convert it to a list of objects
+            var channels = [];
             for (var i = 0, len = keylist.length; i < len; i++) {
                 objectList.push({shortname: keylist[i],
                                  longname:  keylist[i]});
             };
+            for (var i = 0, len = channelKeys.length; i < len; i++) {
+                channels.push({shortname: channelKeys[i],
+                                 longname:  channelKeys[i]});
+            };
             _component.targets = objectList;
+            _component.channels = channels;
         };
         this.messengerInit(_component.clientID);
     }
